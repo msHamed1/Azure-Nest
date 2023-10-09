7b6cc16a174f0d51c7b42ac8374253b5628b1d0b
msHamed1 <mahmoud.hamed@itfaq.global>
2023-10-09 12:11:37 +0400

    fix eslinting errors

--- packages/frontend/src/components/Headers.js
+++ packages/frontend/src/components/Headers.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Layout } from 'antd';
-import { Button, Space } from 'antd';
+import { Button } from 'antd';
 import { Col, Row } from "antd";
 
 const { Header } = Layout;
